7b10d502842bec4b51d586c77af8ddcb0d60c75d
alexrodriguez <alexcarvajal2404@gmail.com>
2023-09-17 08:53:36 -0500

    :zap: log

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -3,6 +3,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // Check for secret to confirm this is a valid request
+    console.log(req.query.secret)
+    console.log(process.env.SECRET_REVALIDATE_TOKEN);
+
+    console.log(req.query.secret === process.env.SECRET_REVALIDATE_TOKEN);
     if (req.query.secret !== process.env.SECRET_REVALIDATE_TOKEN) {
         return res.status(401).json({ message: 'Invalid token' })
     }

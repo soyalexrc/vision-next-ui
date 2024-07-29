b096672c2df374bd770012797829a9cd30553e56
alexrodriguez <alexcarvajal2404@gmail.com>
2024-07-29 12:28:17 -0500

    new unique generated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,9 @@
+import prisma from '@/lib/db/prisma';
 import { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const properties = await prisma.property.findMany();
+  const baseSitemap: MetadataRoute.Sitemap = [
     {
       url: 'https://visioninmobiliaria.com.ve',
       lastModified: new Date(),
@@ -63,4 +65,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.5,
     },
   ];
+  properties.forEach((property) => {
+    baseSitemap.push({
+      url: `${process.env.HOST_URL}/inmuebles/${property.slug}`,
+      lastModified: property.updatedAt,
+    });
+  });
+  return baseSitemap;
 }

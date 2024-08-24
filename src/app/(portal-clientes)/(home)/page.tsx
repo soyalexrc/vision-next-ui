15329f5eba700d523bd86dfa7a2913eeb44a18f9
alexrodriguez <alexcarvajal2404@gmail.com>
2024-08-24 14:25:04 -0500

    latest changes

--- src/app/(portal-clientes)/(home)/page.tsx
+++ src/app/(portal-clientes)/(home)/page.tsx
@@ -4,19 +4,17 @@ import CategoriesCarousel from '@/components/CategoriesCarousel';
 import ContactUsBanner from '@/components/ContactUsBanner';
 import FeaturedProperties from '@/components/FeaturedProperties';
 import ServicesBanner from '@/components/ServicesBanner';
+import { getFeaturedCategoriesCached } from '@/actions/category';
+import { Categories } from '@prisma/client';
 
 export default async function HomePage() {
-  const categories = await fetch(`${process.env.HOST_URL}/api/categories?isFeatured=true`, {
-    cache: 'force-cache',
-    next: { tags: ['categories_home'] },
-    method: 'GET',
-  }).then((res) => res.json());
+  const categories = await getFeaturedCategoriesCached();
 
   return (
     <div className={`flex  min-h-screen flex-col items-center justify-between`}>
       <Banner />
       <Searcher />
-      <CategoriesCarousel categories={categories} />
+      <CategoriesCarousel categories={categories as Categories[]} />
       <ContactUsBanner />
       <FeaturedProperties />
       <ServicesBanner />
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+
 export default function Banner() {
-  return <img className="object-cover w-full h-full min-h-[300px] lg:h-[92vh] " src="/home/induo-inmobiliaria.jpeg" alt="" />;
+  return (
+    <div className="relative w-full">
+      <div className="absolute hidden md:block top-0 left-0 h-full w-full bg-black opacity-50" />
+      <Image className="object-cover w-full h-[300px] md:h-[92vh]" width={200} height={300} src="/home/induo-inmobiliaria.jpeg" alt="" />
+    </div>
+  );
 }

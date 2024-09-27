9a74c497ae7392a802b3e3bbef84aa0445ada1e3
alex rodriguez <alexcarvajal2404@gmail.com>
2024-09-27 00:47:13 -0500

    volver a portal buton, vision type

--- src/app/ingreso/[[...rest]]/page.tsx
+++ src/app/ingreso/[[...rest]]/page.tsx
@@ -2,6 +2,8 @@ import LoginAnimation from '@/components/auth/LoginAnimation';
 import { SignIn } from '@clerk/nextjs';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default async function SignInPage() {
   const user = await currentUser();
@@ -15,7 +17,12 @@ export default async function SignInPage() {
       <div className="hidden lg:flex col-span-7 bg-gray-100 items-center justify-center">
         <p>informacion sobre el sistema (carousel animado, etc...)</p>
       </div>
-      <div className="col-span-12 lg:col-span-5 p-12 flex justify-center">
+      <div className="col-span-12 lg:col-span-5 p-12 flex justify-center relative">
+        <Link href="/">
+          <Button variant="vision" className="absolute top-0 left-0 right-0">
+            Volver a el portal inmobiliario
+          </Button>
+        </Link>
         <div>
           <div className="flex justify-center">
             <LoginAnimation />

e59af5c414f1fc6fc19e035cfe1e28ea5c7f0fdb
alex rodriguez <alexcarvajal2404@gmail.com>
2024-09-27 00:47:07 -0500

    volver a portal buton, vision type

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,6 +15,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        vision: 'bg-red-900 hover:bg-red-900/90 text-primary-foreground',
       },
       size: {
         default: 'h-10 px-4 py-2',

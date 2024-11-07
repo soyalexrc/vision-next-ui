bb6a14442388be5d945ca9ab5cbaa898cf9ac5d4
alexrodriguez <alexcarvajal2404@gmail.com>
2024-11-06 20:04:37 -0500

    :zap: fix unused values

--- src/components/property/FiltersConfig.tsx
+++ src/components/property/FiltersConfig.tsx
@@ -1,38 +1,36 @@
 'use client';
 import React, { useState } from 'react';
-import { LOCATIONS, LOCATIONS_DETAIL } from '@/utils/data/locations';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
 import { PROPERTY_TYPES } from '@/utils/data/property-types';
-import { Button } from '@/components/ui/button';
 
 export function FiltersConfig() {
-  const [state, setState] = useState<string>('');
-  const [municipality, setMunicipality] = useState<string>('');
-  const [municipalitiesList, setMunicipalitiesList] = useState<string[]>([]);
+  // const [state, setState] = useState<string>('');
+  // const [municipality, setMunicipality] = useState<string>('');
+  // const [municipalitiesList, setMunicipalitiesList] = useState<string[]>([]);
   const [propertyType, setPropertyType] = useState<string>('');
   const [operationType, setOperationType] = useState<string>('');
 
-  const updateQuery = () => {
-    // const updatedSearchParams = new URLSearchParams(searchParams?.toString());
-    // updatedSearchParams.set('pagina', encodeURI(currentPage));
-    // updatedSearchParams.set('limite', encodeURI(pageLimit));
-    // updatedSearchParams.set('estado', encodeURI(state));
-    // updatedSearchParams.set('municipalidad', encodeURI(municipality));
-    // updatedSearchParams.set('tipo_de_operacion', encodeURI(operationType));
-    // updatedSearchParams.set('tipo_de_inmueble', encodeURI(propertyType));
-    //
-    // router.push('/inmuebles' + '?' + updatedSearchParams.toString());
-  };
+  // const updateQuery = () => {
+  // const updatedSearchParams = new URLSearchParams(searchParams?.toString());
+  // updatedSearchParams.set('pagina', encodeURI(currentPage));
+  // updatedSearchParams.set('limite', encodeURI(pageLimit));
+  // updatedSearchParams.set('estado', encodeURI(state));
+  // updatedSearchParams.set('municipalidad', encodeURI(municipality));
+  // updatedSearchParams.set('tipo_de_operacion', encodeURI(operationType));
+  // updatedSearchParams.set('tipo_de_inmueble', encodeURI(propertyType));
+  //
+  // router.push('/inmuebles' + '?' + updatedSearchParams.toString());
+  // };
 
-  function handleChangeLocation(value: string) {
-    setState(value);
-    if (value === '') setMunicipalitiesList([]);
-    if (value === 'Caracas') setMunicipalitiesList(LOCATIONS_DETAIL.caracas);
-    if (value === 'Carabobo') setMunicipalitiesList(LOCATIONS_DETAIL.carabobo);
-    if (value === 'Cojedes') setMunicipalitiesList(LOCATIONS_DETAIL.cojedes);
-    if (value === 'Aragua') setMunicipalitiesList(LOCATIONS_DETAIL.aragua);
-  }
+  // function handleChangeLocation(value: string) {
+  //   setState(value);
+  //   if (value === '') setMunicipalitiesList([]);
+  //   if (value === 'Caracas') setMunicipalitiesList(LOCATIONS_DETAIL.caracas);
+  //   if (value === 'Carabobo') setMunicipalitiesList(LOCATIONS_DETAIL.carabobo);
+  //   if (value === 'Cojedes') setMunicipalitiesList(LOCATIONS_DETAIL.cojedes);
+  //   if (value === 'Aragua') setMunicipalitiesList(LOCATIONS_DETAIL.aragua);
+  // }
   return (
     <div className="my-5 flex gap-4">
       {/*<div>*/}

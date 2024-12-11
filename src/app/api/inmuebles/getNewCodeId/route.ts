b9a5309df0993297bf058d54e7559f72988c4e39
alexrodriguez <alexcarvajal2404@gmail.com>
2024-12-11 13:04:59 -0500

    :bug: intento de solucion de atascamiento de autogeneracion de codigos

--- src/app/api/inmuebles/getNewCodeId/route.ts
+++ src/app/api/inmuebles/getNewCodeId/route.ts
@@ -1,11 +1,12 @@
 import prisma from '@/lib/db/prisma';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
+    console.log(req.json());
     let newVinmId: string = '';
     const amountOfProperties = await prisma.property.count();
-    console.log('amountOfProperties', amountOfProperties);
+    console.warn('amountOfProperties', amountOfProperties);
     switch (String(amountOfProperties).length) {
       case 1:
         newVinmId = `VINM_00${amountOfProperties + 1}`;

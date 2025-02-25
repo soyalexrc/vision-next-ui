9b9bf88114021d9cd632a85c886db36e1ba7cdb0
Alex Rodriguez <alexcarvajal2404@gmail.com>
2025-02-25 08:52:38 -0500

    filtros para inmuebles

--- src/app/administracion/inmuebles/page.tsx
+++ src/app/administracion/inmuebles/page.tsx
@@ -1,13 +1,35 @@
 'use client';
+import { useState } from 'react';
 import { columns } from '@/components/property/admin/table';
-import { AlertTriangle } from 'lucide-react';
+import {AlertTriangle, Plus} from 'lucide-react';
 import { TableSkeleton } from '@/components/ui/table-skeleton';
 import { useProperties } from '@/lib/api/properties';
 import { DataTable } from '@/components/ui/data-table';
 import { TableFilters } from '@/components/property/admin';
+import {Button} from "@/components/ui/button";
+import {useRouter} from "next/navigation";
 
 export default function Page() {
   const { data, isPending, error } = useProperties();
+  const [query, setQuery] = useState('');
+  const [operationType, setOperationType] = useState('');
+  const [propertyType, setPropertyType] = useState('');
+  const router = useRouter();
+
+  // Filter data based on the query
+  const filteredData = data?.filter((property) => {
+    const searchText = query.toLowerCase();
+
+    return (
+      (!query ||
+        property.code?.toLowerCase().includes(searchText) ||
+        property.publicationTitle?.toLowerCase().includes(searchText) ||
+        property.propertyType?.toLowerCase().includes(searchText) ||
+        property.operationType?.toLowerCase().includes(searchText)) &&
+      (operationType === 'all' || !operationType || property.operationType === operationType) &&
+      (propertyType === 'all' || !propertyType || property.propertyType === propertyType)
+    );
+  });
 
   return (
     <>
@@ -16,11 +38,25 @@ export default function Page() {
         Trabajo en progreso - Esta pagina se encuentra bajo desarrollo activo.
       </div>
       <div className="p-4 container mx-auto">
-        <h1 className="text-4xl mb-4">Inmuebles</h1>
-        <TableFilters />
+        <div className="flex  justify-between">
+          <h1 className="text-4xl mb-4">Inmuebles</h1>
+          {/* Button to Add a New Property */}
+          <Button variant="outline" className="flex gap-2" onClick={() => router.push('inmuebles/nuevo')}>
+            <Plus />
+            Nuevo inmueble
+          </Button>
+        </div>
+        <TableFilters
+          query={query}
+          operationType={operationType}
+          setOperationType={setOperationType}
+          propertyType={propertyType}
+          setPropertyType={setPropertyType}
+          setQuery={setQuery}
+        />
         {isPending && <TableSkeleton />}
         {error && <div>Error: {error.message}</div>}
-        {data && <DataTable columns={columns} data={data} />}
+        {data && <DataTable columns={columns} data={filteredData!} />}
       </div>
     </>
   );
--- src/components/property/admin/TableFilters.tsx
+++ src/components/property/admin/TableFilters.tsx
@@ -1,34 +1,65 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { Button } from '@/components/ui/button';
-import { Plus } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useCategories } from '@/lib/api/categories';
+import React from 'react';
 
-export function TableFilters() {
-  const router = useRouter();
-  // const pathname = usePathname();
-  // const searchParams = useSearchParams();
-  // const [query, setQuery] = useState<string>(searchParams.get('busqueda') ?? '');
-
-  // function search() {
-  //   const params = new URLSearchParams(searchParams.toString());
-  //   params.set('busqueda', query);
-  //   router.push(pathname + '?' + params.toString());
-  // }
+export function TableFilters({
+  query,
+  setQuery,
+  operationType,
+  setOperationType,
+  propertyType,
+  setPropertyType,
+}: {
+  query: string;
+  setQuery: (value: string) => void;
+  operationType: string;
+  setOperationType: (value: string) => void;
+  propertyType: string;
+  setPropertyType: (value: string) => void;
+}) {
+  const { data } = useCategories();
 
   return (
-    <div className="flex flex-col md:flex-row mb-4 gap-4">
-      {/*<Input*/}
-      {/*  placeholder="Buscar por codigo o titulo, tipo de propiedad u operacion"*/}
-      {/*  value={query}*/}
-      {/*  onChange={({ target: { value } }) => setQuery(value)}*/}
-      {/*  className="w-full md:max-w-sm"*/}
-      {/*/>*/}
-      {/*<Button onClick={search}>Buscar</Button>*/}
-      <span className="mx-auto h-[30px] md:h-auto"></span>
-      <Button variant="outline" className="flex gap-2" onClick={() => router.push('inmuebles/nuevo')}>
-        <Plus />
-        Nuevo inmueble
-      </Button>
+    <div className="flex flex-col md:flex-row mb-4 md:items-end gap-4">
+      {/* Search Input */}
+      <Input placeholder="Buscar por código o título" className="w-full" value={query} onChange={(e) => setQuery(e.target.value)} />
+
+      {/* Select for Operation Type */}
+      <div>
+        <p className="text-xs font-bold mb-1">Tipo de operacion</p>
+        <Select value={operationType} onValueChange={setOperationType}>
+          <SelectTrigger className="w-full md:w-40">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">Todos</SelectItem>
+            <SelectItem value="Venta">Venta</SelectItem>
+            <SelectItem value="Alquiler">Alquiler</SelectItem>
+            <SelectItem value="Traspaso">Traspaso de fondo</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {/* Select for Property Type */}
+      <div>
+        <p className="text-xs font-bold mb-1">Tipo de propiedad</p>
+
+        <Select value={propertyType} onValueChange={setPropertyType}>
+          <SelectTrigger className="w-full md:w-40">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">Todos</SelectItem>
+            {data?.map((property) => (
+              <SelectItem key={property.id} value={property.title}>
+                {property.title}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 }
